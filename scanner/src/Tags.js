8e0fdb9180e775ae52c99d7c27e1a6a3a1667143
anas-dev0 <anas.aouini@supcom.tn>
2025-05-02 19:03:47 +0100

    aaa

---
+++ scanner/src/Tags.js
@@ -0,0 +1,34 @@
+import Chip from "@mui/material/Chip";
+
+function AcceptedTag() {
+  return (
+    <Chip
+      style={{ zoom: "1.5", margin: "10px" }}
+      label={"Welcome"}
+      color={"success"}
+      variant="contained"
+    />
+  );
+}
+function RefusedTag() {
+  return (
+    <Chip
+      style={{ zoom: "2" }}
+      label={"Unvalid-token"}
+      color={"error"}
+      variant="contained"
+    />
+  );
+}
+function AllreadyAcceptedTag() {
+  return (
+    <Chip
+      style={{ zoom: "2" }}
+      label={"Already-checked"}
+      color={"warning"}
+      variant="contained"
+    />
+  );
+}
+
+export { AcceptedTag, RefusedTag, AllreadyAcceptedTag };
